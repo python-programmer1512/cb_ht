38d334f5cb10e9bcf448a2d0fd09a330f39d92e1
python-programmer1512 <joseph13ko15@gmail.com>
2022-11-20 19:18:42 +0900

    jj

---
+++ Object_detection.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+# Yolo 로드
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# 이미지 가져오기
+img = cv2.imread("sample.jpg")
+img = cv2.resize(img, None, fx=0.4, fy=0.4)
+height, width, channels = img.shape
+
+# Detecting objects
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
